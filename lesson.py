4bde905043509bf3ca722de9231ed0988f53d304
evgeniyryzhkin <jmaster.me@gmail.com>
2021-06-10 01:09:56 +0300

    lesson

---
+++ lesson.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    x = float(input())
+    if x > 0:
+        res = 2 * x - 1
+    elif x < 0:
+        res = x - 2
+    else:
+        res = 0
+    print(res)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
