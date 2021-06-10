21d4d22136a4e3be17d972a9bdcc3a799842d212
evgeniyryzhkin <jmaster.me@gmail.com>
2021-06-10 17:11:34 +0300

    task 3 upd

--- task_3.py
+++ task_3.py
@@ -1,5 +1,15 @@
 if __name__ == '__main__':
-    k = input("Введите первую точку:\n")
-    b = input("Введите вторую точку:\n")
+    x1, y1 = (float(i) for i in input("Введите первую точку (x,y):\n").split(','))
+    x2, y2 = (float(i) for i in input("Введите вторую точку (x,y):\n").split(','))
 
-    print(f"Уравнение прямой, проходящей через эти точки: y={k}x+{b}")
+    if x1 == x2 or y1 == y2:
+        print("Значения x двух точек не должны совпадать.")
+    else:
+        k = (y1 - y2) / (x1 - x2)
+        b = y1 - k * x1
+        k = int(k) if k - int(k) == 0 else k
+        b = int(b) if b - int(b) == 0 else b
+
+        print(f"Уравнение прямой, проходящей через эти точки: y={k}*x + {b}")
+
+
